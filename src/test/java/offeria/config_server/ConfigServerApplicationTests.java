f0ed107007453d28d50f9116e01097713044bfd9
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-04-03 17:15:28 +0300

    Set active profile to 'dev' in ConfigServerApplicationTests for testing purposes

--- src/test/java/offeria/config_server/ConfigServerApplicationTests.java
+++ src/test/java/offeria/config_server/ConfigServerApplicationTests.java
@@ -2,12 +2,15 @@ package offeria.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev") // Use dev profile for testing which uses a public sample repo
 class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Basic test to verify Spring context loads correctly
 	}
 
 }
